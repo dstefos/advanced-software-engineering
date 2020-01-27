8f0b8c4174f5c4b768e1ab22a03b682a434aa692
Pantelis-Arsenios Kamanas <parkamanas@gmail.com>
2020-01-27 17:12:34 +0200

    Initialization 7

--- src/PockedexScreen.java
+++ src/PockedexScreen.java
@@ -39,6 +39,7 @@ public class PockedexScreen extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public PockedexScreen() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
